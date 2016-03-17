f75dbcf8de42e8c098ba2e184989dfae80f63fd3
raitisd <rd@itembase.biz>
2016-03-17 12:16:28 +0100

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='api-swagger',
+      version='1.0',
+      description='Swagger documentation for Itembas API',
+      author='John Wiesel',
+      author_email='jw@itembase.biz',
+      url='https://www.itembase.com/',
+      packages=['api-swagger'],
+     )
+
